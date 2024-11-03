a7aa6d83209366f568dfdb297c33b66e2f544dad
Itslush <135375747+Itslush@users.noreply.github.com>
2024-11-04 00:28:33 +0200

    Replaced ToggleInputMode() function
    I forgot I added it initially as a placeholder oops

--- InputHandler.cs
+++ InputHandler.cs
@@ -1,59 +1,47 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using static EternalFracture.NetworkManagement;
+using static EternalFracture.NetworkManagementWMI;
 using static EternalFracture.ConsoleLogging;
 
 namespace EternalFracture
 {
-    internal class InputHandler
+    class InputHandler
     {
         public static bool UseAsyncKeyState = true;
+        private static DateTime _lastToggleTime = DateTime.MinValue;
+        private static readonly TimeSpan ToggleCooldown = TimeSpan.FromSeconds(1);
         public static Dictionary<int, bool> KeyProcessed { get; } = new()
         {
-            { 0x44, false }, { 0x45, false }, { 0x4C, false }, { 0x51, false }, { 0x54, false }
+            {0x43, false },{ 0x44, false }, { 0x45, false }, { 0x4C, false }, { 0x51, false }, { 0x54, false }, {0x53, false}, {0x52, false}
         };
-
         public static Dictionary<ConsoleKey, Action> KeyActions { get; } = new()
         {
-            { ConsoleKey.D, () => NetworkManagement.DisableAllNetworkInterfaces() },
-            { ConsoleKey.E, NetworkManagement.EnableAllNetworkInterfaces },
+            { ConsoleKey.D, NetworkManagementWMI.DisableAllNetworkInterfaces},
+            { ConsoleKey.E, NetworkManagementWMI.EnableAllNetworkInterfaces },
             { ConsoleKey.L, ToggleLoggingLevel },
             { ConsoleKey.Q, () => { Log("Exiting...", ErrorColor); Environment.Exit(0); } },
             { ConsoleKey.T, InputHandler.ToggleInputMode },
-            { ConsoleKey.S, NetworkManagement.SetDisableDuration },
-            { ConsoleKey.R, () => NetworkManagement.TempDisableNetInterfaces(NetworkManagement.disableDurationSeconds) }
+            { ConsoleKey.S, NetworkManagementWMI.SetDisableDuration },
+            { ConsoleKey.R, () => NetworkManagementWMI.TempDisableNetInterfaces(NetworkManagementWMI.disableDurationSeconds) },
+            { ConsoleKey.C, ConsoleLogging.ClearLogs},
         };
-
-        [DllImport("User32.dll", EntryPoint = "GetAsyncKeyState")]
-        private static extern short GetAsyncKeyState(int vKey);
-
         public static void CheckAsyncKeyState()
         {
-            CheckKeyState(0x44, () => NetworkManagement.DisableAllNetworkInterfaces());
-            CheckKeyState(0x45, NetworkManagement.EnableAllNetworkInterfaces);
+            CheckKeyState(0x43, ConsoleLogging.ClearLogs);
+            CheckKeyState(0x44, NetworkManagementWMI.DisableAllNetworkInterfaces);
+            CheckKeyState(0x45, NetworkManagementWMI.EnableAllNetworkInterfaces);
             CheckKeyState(0x4C, ConsoleLogging.ToggleLoggingLevel);
-            CheckKeyState(0x51, () =>
-            {
-                ConsoleLogging.Log("Exiting...", ConsoleLogging.ErrorColor);
-                Environment.Exit(0);
-            });
+            CheckKeyState(0x51, () => { ConsoleLogging.Log("Exiting...", ConsoleLogging.ErrorColor); Environment.Exit(0); });
             CheckKeyState(0x54, ToggleInputMode);
+            CheckKeyState(0x53, SetDisableDuration);
+            CheckKeyState(0x52, () => TempDisableNetInterfaces(NetworkManagementWMI.disableDurationSeconds));
         }
-
-        public static void CheckConsoleReadKey()
-        {
-            var key = Console.ReadKey(true).Key;
-
-            if (KeyActions.TryGetValue(key, out var action))
-            {
-                action();
-            }
-        }
-
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
         private static void CheckKeyState(int vKey, Action action)
         {
             if ((GetAsyncKeyState(vKey) & 0x8000) > 0)
@@ -69,28 +57,22 @@ namespace EternalFracture
                 KeyProcessed[vKey] = false;
             }
         }
+        public static void CheckConsoleReadKey()
+        {
+            var keyInfo = Console.ReadKey(intercept: true).Key;
 
+            if (KeyActions.TryGetValue(keyInfo, out var action))
+            {
+                action();
+            }
+        }
         private static void ToggleInputMode()
         {
-            ConsoleLogging.Log("Toggling Input Mode...", ConsoleLogging.WarningColor);
+            if (DateTime.Now - _lastToggleTime < ToggleCooldown) return;
+
+            _lastToggleTime = DateTime.Now;
             UseAsyncKeyState = !UseAsyncKeyState;
+            Log($"Input mode toggled. Using {(UseAsyncKeyState ? "GetAsyncKeyState" : "Console.ReadKey")} for input.", InfoColor);
         }
-
-        /*private static void LogWithGradient(string message)
-        {
-            var gradientColors = new[] { ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Green };
-            var messageLength = message.Length;
-            var colorStep = (double)messageLength / gradientColors.Length;
-
-            for (int i = 0; i < messageLength; i++)
-            {
-                var colorIndex = (int)(i / colorStep);
-                Console.ForegroundColor = gradientColors[colorIndex];
-                Console.Write(message[i]);
-            }
-
-            Console.WriteLine();
-            Console.ResetColor();
-        }*/
     }
 }
